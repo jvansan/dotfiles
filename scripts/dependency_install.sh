690b177acb0cc11738255f0222b45e6361be3c25
jvansan <jeffreyavansanten@gmail.com>
2022-08-04 12:07:53 -0700

    change to fedora dependency management

--- scripts/dependency_install.sh
+++ scripts/dependency_install.sh
@@ -37,40 +37,24 @@ fzf_install() {
 #   echo "Finished installing rainbarf locally"
 # }
 
+install_fedora() {
+  sudo dnf install -y vim neovim python3-neovim flameshot
+}
+
 install_ohmyzsh() {
   echo "Installing ohmyzsh..."
   if [ ! -d $HOME/.oh-my-zsh/ ]; then
-    curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
+    curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh | bash
   else
     echo "ohmyzsh already installed"
   fi
 }
 
 
-NEOVIM_DIR=$HOME/git/neovim
-echo $NEOVIM_DIR
-neovim_install() {
-  echo "Installing neovim..."
-  if ! [ -d $NEOVIM_DIR ]; then
-    echo "Installing neovim..."
-    git clone https://github.com/neovim/neovim $NEOVIM_DIR
-    cd ~/neovim
-    make CMAKE_EXTRA_FLAGS="-DCMAKE_INSTALL_PREFIX=$NEOVIM_DIR"
-    make install
-    cd -
-
-    # install python dependencies
-    python3 -m pip install --upgrade pynvim
-  else
-    echo "neovim already installed"
-  fi
-  echo "Finished installing neovim locally"
-}
-
 install() {
+  install_fedora
   fzf_install
   install_ohmyzsh
-	neovim_install
 }
 
 create_ssh
