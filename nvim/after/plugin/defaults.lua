ce78b266645084ffd5671de163b87dc53da99fd2
jvansan <jeffreyavansanten@gmail.com>
2022-11-16 12:40:01 -0800

    update netrw conf

--- nvim/after/plugin/defaults.lua
+++ nvim/after/plugin/defaults.lua
@@ -31,6 +31,7 @@ g.netrw_banner = 0 -- Hide banner
 g.netrw_browse_split = 4 -- Open in previous window
 g.netrw_altv = 1 -- Open with right splitting
 g.netrw_liststyle = 3 -- Tree-style view
+g.netrw_winsize = 25 -- default netrw window size
 g.netrw_list_hide = (vim.fn["netrw_gitignore#Hide"]()) .. [[,\(^\|\s\s\)\zs\.\S\+]] -- use .gitignore
 
 -- Highlight on yank
